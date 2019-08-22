c521fa566618dc2232cf30083efc894441f8f2a7
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-08-22 21:08:03 +0300

    Day three

---
+++ Day3.py
@@ -0,0 +1,19 @@
+# variables
+
+x = 5 # type of x is integer
+print(x)
+
+x = "Amani" # Now type of x is string
+x = 'Amani' # can use single or double quotes
+# variable names are case sensitive
+X = 'Alshami'
+
+# Concatenate strings
+print("Name : "+x + X)
+
+
+#Assign Value to Multiple Variables
+y , z = 3, 4
+print(y + z)
+
+a = b = 0
