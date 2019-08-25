e587b77a7521e8add4680c20deee50ee17ab62e9
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-08-25 14:16:18 +0300

    Day6

---
+++ Day6- Casting.py
@@ -0,0 +1,17 @@
+# Casting 
+
+# convert to integer number by using int()
+print("Integer number: ")
+print(int("6"))
+print(int(6.9))
+
+# convert to floting number by using float()
+print("Floting number : ")
+print(float(6))
+print(float("6.9"))
+
+# convert to String by using str()
+print("String : ")
+
+print(str(6))
+print(str(6.9))
