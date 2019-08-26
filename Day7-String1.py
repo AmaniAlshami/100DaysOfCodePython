a0a4bdc011dd473d54a4c0be258d0978b0adac04
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-08-26 14:32:29 +0300

    Day7

---
+++ Day7-String1.py
@@ -0,0 +1,22 @@
+# String 
+
+a = "Hello "
+b = 'World '
+print(a+b)
+
+c = '''
+Multiline string 
+We can assign a multiline string to a
+variable by using three quotes
+'''
+print(c)
+
+d = "Amani Alshami"
+# Strings are arrays , every character has index .
+print(d[0]) 
+print(d[6:])
+
+
+
+
+
