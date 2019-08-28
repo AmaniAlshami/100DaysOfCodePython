a406f7ed98001c99f6c9cac2f72d3a9e235671e2
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-08-28 23:15:02 +0300

    Day9

---
+++ Day9-StringFormat.py
@@ -0,0 +1,16 @@
+# using format() to inser value of variable in String 
+
+
+name = 'Amani'
+age = 21 
+job = 'Student'
+
+print("Name : {} \nAge : {} \nJob : {}".format(name,age,job))
+
+# anthor way :
+
+print(f"Name : {name} \nAge : {age} \nJob : {job}")
+
+
+
+
