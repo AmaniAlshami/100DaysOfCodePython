12ccfa6c1f0c554a4767e923e1cd9a58fb39723c
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-01 23:56:43 +0300

    Day13

---
+++ Day13-Lists.py
@@ -0,0 +1,25 @@
+# Number 
+list1 = [1,2,3,4]
+# String
+list2 = ["Yellwo","Red","Green"]
+# String and Number 
+list3 = list1 + list2
+
+print(list3)
+
+# use for loop tp print all items in the list , and can be do any opretor on items
+for x in list1:
+    print(x*2)
+
+list2[2] = "Blue"
+print(list2)
+
+# delete items 
+del list3[0:4]
+print(list3)
+
+# or can use del to delete list 
+
+del list3
+
+    
