a90f15af8569892e83ff1b0e20262a384459616a
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-07 23:03:30 +0300

    Day16

---
+++ Day16-Tuples1.py
@@ -0,0 +1,16 @@
+# Tuples : is a collection which is ordered and unchangeable
+
+color = ("Red","Green","Yellow")
+
+for i in color:
+    print(i)
+
+
+Student=(1,"Ahmad",98.9)
+print("Student Information ")
+print("ID: " + str(Student[0])+"\nName: "+Student[1]+"\nGrade: " + str(Student[2]))
+
+Number = (1,2,3)
+print(Number)
+
+
