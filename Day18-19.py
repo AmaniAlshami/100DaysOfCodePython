86fe143a269eb93e690d1db1f848814329cb19fa
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-07 23:30:19 +0300

    exercise

---
+++ Day18-19.py
@@ -0,0 +1,15 @@
+Names =['Ahamd','Fahad','Saleh','Ali']
+print(Names)
+print(Names.count("Ali"))
+
+StudentsNames = Names.copy()
+StudentsNames.append("Khaled")
+StudentsNames.sort()
+print(StudentsNames)
+
+programmingLang = ('java', 'python', 'swift')
+if 'python' in programmingLang:
+    print("Yes , Python is progrmaing Language .")
+
+
+
