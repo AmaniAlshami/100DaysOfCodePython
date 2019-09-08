4521eaf8c353eb3f0a89ec2004979f1deff94c33
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-08 21:27:10 +0300

    Set

---
+++ Day20-Sets1.py
@@ -0,0 +1,14 @@
+# Set is unordered 
+# values must be uniqe 
+colors ={'Red','Blue','Pink','Red'} 
+print(colors) # print one 'Red' 
+
+
+print('Red' in colors)
+
+colors.add('Black') # ues add() to insert one item
+colors.update(['Green','Yellow']) # use update() to insert multitems .
+
+for i in colors:
+    print(i)
+
