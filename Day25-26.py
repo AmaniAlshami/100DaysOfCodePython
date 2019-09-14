747b8b59d90ddb417edc99f5240c3433cf2ac368
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-14 23:46:10 +0300

    exercise

---
+++ Day25-26.py
@@ -0,0 +1,25 @@
+set1 = {1,3,5,7,8}
+# add (4,8,12)
+set1.update([4,8,12])
+print(set1)
+# remove '8'
+set1.remove(8)
+print(set1)
+# delete all items
+set1.clear()
+print(set1)
+
+#######################
+
+# create dictionary 
+dic1 = {'name':'pigeon','type':'bird','skin cover':'feathers'}
+print(dic1)
+
+print(dic1['type'])
+
+dic1['skin cover'] = 'Plume'
+
+
+
+
+
