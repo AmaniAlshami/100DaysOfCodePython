0722d09eb65fa75ad05379ef2c002ad3ea7bf52b
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-14 23:36:12 +0300

    Day24

---
+++ Day24-Dictionary3.py
@@ -0,0 +1,22 @@
+Student = dict(Name='Amani',ID=1,Mark=100)
+print(Student)
+# copy a dictionary
+Student2 = Student.copy()
+print(Student2)
+# Nested Dictionaries
+Mycourse = {
+    "Course1" :{
+        'CourseID' : 222,
+        'CourseName' : 'Math'
+    },
+    "Course2" :{
+        'CourseID' : 333,
+        'CourseName' : 'Programming'
+    },
+    "Course3" :{
+        'CourseID' : 444,
+        'CourseName' : 'English'
+    }
+}
+
+print(Mycourse)
