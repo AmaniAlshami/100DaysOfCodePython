37afc124667cc8ada99f06a2a28e67456499450e
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-17 22:06:37 +0300

    While Loop

---
+++ Day28-WhileLoop.py
@@ -0,0 +1,25 @@
+# print 5 numbers 
+i = 1
+while i < 10:
+    print(i)
+    if i == 5 :
+        break
+    i +=1
+print("__________________")
+# skip number 5
+j=0  
+while j < 10:
+    j+=1
+    if j == 5 :
+       continue
+    print(j)
+
+print("__________________")
+a = 1
+while a <= 10:
+    print(a)
+    a+=1
+else:
+    print(f"{a} is greater than 10")
+
+    
