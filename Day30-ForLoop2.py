d3341c3587975b689fec232f62bb0784e7700ed0
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-09-19 00:23:16 +0300

    For Loop

---
+++ Day30-ForLoop2.py
@@ -0,0 +1,16 @@
+# print even number 0 - 100
+
+for x in range(0,101,2):
+    print(x)
+
+# nested for loop 
+# print pattren 
+for y in range(6):
+    for x in range(y):
+      print("* ",end="")
+    print("")
+
+    
+
+
+
