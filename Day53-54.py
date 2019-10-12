ac05d872ce8ae7520ea42252bff28ad62239946b
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-10-12 22:18:52 +0300

    exercise

---
+++ Day53-54.py
@@ -0,0 +1,25 @@
+#import Calculate as c 
+
+
+#print("1 + 8 = ",c.sum(8,1))
+#print("4 - 2 = ",c.sub(4,2))
+#print("6 * 6 = ",c.mult(6,6))
+#print("8 / 2 = ",c.dev(8,2))
+
+
+###########################
+
+import datetime 
+
+time = datetime.datetime.now()
+print(time)
+print(time.year)
+print(time.month)
+print(time.day)
+
+###########################
+yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+print("yesterday : " ,yesterday)
+
+tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+print("tomorrow : " ,tomorrow)
