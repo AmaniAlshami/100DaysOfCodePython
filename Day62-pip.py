5e592d744503f3b48d9931a1c5b2ea6abaca91dc
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-10-21 23:01:23 +0300

    pip

---
+++ Day62-pip.py
@@ -0,0 +1,10 @@
+# pip3 install camelcase 
+import camelcase 
+
+c = camelcase.CamelCase()
+txt = "lorem ipsum dolor sit amet"
+print(c.hump(txt))
+
+# pip3 uninstall camelcase 
+
+# pip3 list > list all the packages installed on your system.
