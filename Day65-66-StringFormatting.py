01c20b0aa12fecb54f8cf301f2670179d3db4b99
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-11-01 23:46:18 +0300

    String formatting

---
+++ Day65-66-StringFormatting.py
@@ -0,0 +1,6 @@
+name = input("Your name : ")
+age = int(input("Your age : "))
+
+print("Hello my name is {1} and I am {0} years old .".format(age,name)) # index number
+print("Hello my name is {name} and I am {age} years old .".format(age = age ,name = name )) # Named Indexes
+
