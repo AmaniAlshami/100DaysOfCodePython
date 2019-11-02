a07fa8c0c840cd2932e9d8c531da8a8196a59879
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-11-02 18:26:55 +0300

    MySQL

---
+++ Day72.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="mydatabase"
+)
+
+ # order by 
+mycursor = mydb.cursor()
+
+# mycursor.execute("Select * from customers order by name ")
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+
+# mycursor.execute("Select * from customers order by name DESC")
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+# # delete 
+
+# sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
+# mycursor.execute(sql)
+# mydb.commit()
+# print(mycursor.rowcount,"record deleted ")
+# drop 
+# mycursor.execute("DROP TABLE customers")
+
+mycursor.execute("DROP TABLE IF EXISTS customers")
+
