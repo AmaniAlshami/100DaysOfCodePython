40489234cedeeb2a14522fc6b504347ae64ed8ea
Amani Alshami <amanialshami@Amanis-MacBook-Pro-2.local>
2019-11-02 17:58:20 +0300

    MySQL

---
+++ Day70.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="mydatabase"
+)
+
+print(mydb)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(225) , address VARCHAR(225))")
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
